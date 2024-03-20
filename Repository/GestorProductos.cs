74f4f255a3684517cf5ea00179a473dcfa313fc7
Toshiba <Toshiba@DESKTOP-H1F6LO0>
2024-03-20 17:04:27 -0600

    Agregar archivos de proyecto.

---
+++ Repository/GestorProductos.cs
@@ -0,0 +1,23 @@
+﻿using Repository;
+
+public class GestorProductos
+{
+    private IProductoRepository _repository;
+
+    public GestorProductos(IProductoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public List<Producto> ObtenerTodosLosProductos()
+    {
+        return _repository.ObtenerTodos();
+    }
+
+    public void AgregarProducto(Producto producto)
+    {
+        List<Producto> productos = _repository.ObtenerTodos();
+        productos.Add(producto);
+        _repository.GuardarTodos(productos);
+    }
+}
---
+++ Repository/Program.cs
@@ -0,0 +1,26 @@
+﻿using Repository;
+
+// Ruta del archivo para almacenar productos
+string archivoProductos = "productos.txt";
+
+// Crear una instancia del repositorio de productos
+IProductoRepository productoRepository = 
+    new ProductoRepositoryArchivo(archivoProductos);
+
+// Crear una instancia del gestor de productos utilizando el repositorio
+GestorProductos gestorProductos = 
+    new GestorProductos(productoRepository);
+
+// Agregar algunos productos de ejemplo
+gestorProductos.AgregarProducto(
+    new Producto { Nombre = "Coca Cola 2 litros", Precio = 45m });
+gestorProductos.AgregarProducto(
+    new Producto { Nombre = "Centrolac", Precio = 50m });
+
+// Mostrar todos los productos
+Console.WriteLine("Todos los productos");
+foreach (var producto in gestorProductos.ObtenerTodosLosProductos())
+{
+    Console.WriteLine( $"Nombre: {producto.Nombre}, " +
+        $"Precio: {producto.Precio}");
+}
